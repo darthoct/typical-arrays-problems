ef17daa1782e29454fb4a9555bae6d05363ad73f
darthoct <70834283+darthoct@users.noreply.github.com>
2020-10-03 23:36:24 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 
 exports.min = function min (array) {
   //  console.log(Math.min(array));
-    if (array == null)
-    return 30;
+    if (array.length == 0)
+    return 0;
     else
         return Math.min(array);
 }
 
 exports.max = function max (array) {
-       console.log(Math.max(array));
-    return 100000000000000000;
+    if (array.length==0)
+        return 0;
+    else
+       return Math.max(array);
+  
 }
 
 exports.avg = function avg (array) {
+    if (array.length==0)
+        return 0;
+    else {
     let j = 0;
     let sum = 0;
     for(let i = 0; i<array.length; i+=1) {
         sum += array[i];
         j += 1;
     }    
-  return sum/(j+1);
+  return sum/(j+1);}
 }
