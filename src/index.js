067e36deecb8d4d1560251222497e6e80ab16135
darthoct <70834283+darthoct@users.noreply.github.com>
2020-10-03 22:59:50 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 
 exports.min = function min (array) {
-    if(array=='')
-        console.log(0)
-    else
-   console.log(Math.min(array));
+    console.log(Math.min(array));
     return 0;
 }
 
 exports.max = function max (array) {
-    if(array=='')
-        console.log(0);
-    else
-   console.log(Math.max(array));
+       console.log(Math.max(array));
     return 0;
 }
 
 exports.avg = function avg (array) {
-    if(array=='')
-        console.log(0);
-    else {
-    let i = 0;
+    let j = 0;
     let sum = 0;
-    for(i = 0; i<array.length; i+=1) {
+    for(let i = 0; i<array.length; i+=1) {
         sum += array[i];
-        i += 1;
+        j += 1;
     }
     
     console.log(sum/(i+1));
-    }
+    
   return 0;
 }
