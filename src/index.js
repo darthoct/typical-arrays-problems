d791351337aa00d4f45396916913f2ed9f9aeffb
darthoct <70834283+darthoct@users.noreply.github.com>
2020-10-03 23:55:00 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,11 +30,11 @@ exports.avg = function avg (array) {
     if (array.length===0)
         return 0;
     else {
-    let j = 0;
+  //  let j = 0;
     let sum = 0;
     for(let i = 0; i<array.length; i+=1) {
         sum += array[i];
-        j += 1;
+     //   j += 1;
     }    
-  return sum/(j+1);}
+  return sum/array.length;}
 }
